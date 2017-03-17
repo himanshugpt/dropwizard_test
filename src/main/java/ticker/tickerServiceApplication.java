1d4812b8881a10e8920d38131b66409eae79af02
himanshugpt <himanshu.gupta@mongodb.com>
2017-03-17 15:52:10 -0400

    first commit

---
+++ src/main/java/ticker/tickerServiceApplication.java
@@ -0,0 +1,36 @@
+package ticker;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import ticker.health.HealthChecker;
+import ticker.resources.TickerResource;
+
+public class tickerServiceApplication extends Application<tickerServiceConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new tickerServiceApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "tickerService";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<tickerServiceConfiguration> bootstrap) {
+    }
+
+    @Override
+    public void run(final tickerServiceConfiguration configuration,
+                    final Environment environment) {
+    	final TickerResource resource = new TickerResource(configuration.getTemplate(), 
+    			configuration.getDefaultName());
+    	
+        final HealthChecker healthCheck =
+                new HealthChecker(configuration.getTemplate());
+        environment.healthChecks().register("template", healthCheck);    
+    	environment.jersey().register(resource);
+    }
+
+}
